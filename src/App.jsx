38e4c826a11e45ee0d25a01e2e365717a29fd477
Hitesh <hitesh.k.83080@gmail.com>
2024-01-17 17:31:57 +0530

    stepper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import DatePicker from './DatePicker';
+import DatePicker from "./DatePicker";
 import YourComponent from "./UsePagination.jsx";
 import LineStepper from "./Stepper.jsx";
 
-
-// ! To use Chakra-ui-steps you need to extend the chakra theme 
+// ! To use Chakra-ui-steps you need to extend the chakra theme
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { StepsTheme as Steps } from "chakra-ui-steps";
 
@@ -13,20 +12,44 @@ const theme = extendTheme({
   },
 });
 
+const Content1 = () => <h1>Hello, I am content 1</h1>;
+
+const Content2 = () => <h1>Hello, I am content 2</h1>;
+
+const Content3 = () => <h1>Hello, I am content 3</h1>;
+
+const steps = [
+  {
+    label: "Step 1",
+    description: "Description for Step 1",
+    content: <Content1 />,
+  },
+  {
+    label: "Step 2",
+    description: "Description for Step 2",
+    content: <Content2 />,
+  },
+  {
+    label: "Step 3",
+    description: "Description for Step 3",
+    content: <Content3 />,
+  },
+];
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <div style={{padding:30}} className="App">
-        <DatePicker/>
-      <br/>
-      <br/>
-      <br/>
-      <YourComponent/>
-      <br/>
-      <br/>
-      <br/>
+      <div style={{ padding: 30 }} className="App">
+        <DatePicker />
+        <br />
+        <br />
+        <br />
+        <YourComponent />
+        <br />
+        <br />
+        <br />
         <div>
-          <LineStepper variant={"simple"} />
+          <LineStepper steps={steps} />
         </div>
       </div>
     </ChakraProvider>
--- src/Stepper.jsx
+++ src/Stepper.jsx
@@ -1,68 +1,3 @@
-// import { useColorModeValue } from "@chakra-ui/color-mode";
-// import { Box } from "@chakra-ui/react";
-// import { Button, Flex, Heading } from "@chakra-ui/react";
-// import { Step,Steps, useSteps } from "chakra-ui-steps";
-
-// const steps = [
-//   { label: "Step 1", description: "Business Details" },
-//   { label: "Step 2", description: "Business Info" },
-//   { label: "Step 3", description: "Activate Account" },
-// ];
-
-// export const LineStepper = ({ variant }) => {
-//   const { nextStep, prevStep, reset, activeStep } = useSteps({
-//     initialStep: 0,
-//   });
-//   const isLastStep = activeStep === steps.length - 1;
-//   const hasCompletedAllSteps = activeStep === steps.length;
-//   const bg = useColorModeValue("gray.200", "gray.700");
-//   return (
-//       <Flex flexDir="column" width="100%">
-//         <Steps variant={variant} colorScheme={"green"} activeStep={activeStep}>
-//           {steps.map(({ label, description }, index) => (
-//             <Step colorScheme={"green"} label={label} key={label} description={description}>
-//               <Box sx={{ p: 8, bg, my: 8, rounded: "md" }}>
-//                 <Heading fontSize="xl" textAlign="center">
-//                   Step {index + 1}
-//                 </Heading>
-//               </Box>
-//             </Step>
-//           ))}
-//         </Steps>
-//         {hasCompletedAllSteps && (
-//           <Box sx={{ bg, my: 8, p: 8, rounded: "md" }}>
-//             <Heading fontSize="xl" textAlign={"center"}>
-//               Woohoo! All steps completed! 🎉
-//             </Heading>
-//           </Box>
-//         )}
-//         <Flex width="100%" justify="flex-end" gap={4}>
-//           {hasCompletedAllSteps ? (
-//             <Button size="sm" onClick={reset}>
-//               Reset
-//             </Button>
-//           ) : (
-//             <>
-//               <Button
-//                 isDisabled={activeStep === 0}
-//                 onClick={prevStep}
-//                 size="sm"
-//                 variant="ghost"
-//               >
-//                 Prev
-//               </Button>
-//               <Button size="sm" onClick={nextStep}>
-//                 {isLastStep ? "Finish" : "Save & Next"}
-//               </Button>
-//             </>
-//           )}
-//         </Flex>
-//       </Flex>
-//   );
-// };
-
-// export default LineStepper;
-
 import {
   ChakraProvider,
   extendTheme,
@@ -72,7 +7,7 @@ import {
   HStack,
   Button,
 } from "@chakra-ui/react";
-import {  ChevronRightIcon } from "@chakra-ui/icons";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 
 const customTheme = extendTheme({
@@ -85,24 +20,12 @@ const customTheme = extendTheme({
   },
 });
 
-const CustomStepperHelper = () => {
+const CustomStepperHelper = ({ steps }) => {
   const [activeStep, setActiveStep] = useState(0);
-
-  const steps = [
-    { label: "Step 1", description: "Description for Step 1" },
-    { label: "Step 2", description: "Description for Step 2" },
-    { label: "Step 3", description: "Description for Step 3" },
-  ];
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
   };
-
-  // const handlePrev = () => {
-  //   setActiveStep(activeStep - 1);
-  // };
-
-  console.log(activeStep);
 
   return (
     <>
@@ -139,8 +62,7 @@ const CustomStepperHelper = () => {
                 width={"100%"}
                 borderRadius="4"
                 textAlign="center"
-              >
-              </Box>
+              ></Box>
 
               {/* Step Description */}
               <Box
@@ -148,20 +70,44 @@ const CustomStepperHelper = () => {
                 onClick={() => setActiveStep(index)}
                 ml="0"
               >
-                <Text fontWeight={index === activeStep ? "bold" : "normal"}>
+                <Text
+                  fontWeight={600}
+                  fontSize={12}
+                  color={
+                    index < activeStep
+                      ? "green.500" // Completed steps
+                      : index === activeStep
+                      ? "blue.500" // Active step
+                      : index === steps.length
+                      ? "green.600" // Last step upon completion
+                      : "gray.600"
+                  }
+                >
                   {step.label}
                 </Text>
-                <Text color="gray.500">{step.description}</Text>
+                <Text
+                  fontSize={16}
+                  fontWeight={index === activeStep ? "bold" : "normal"}
+                  color={index === activeStep ? "#1d2939" : "#475467"}
+                >
+                  {step.description}
+                </Text>
               </Box>
             </Box>
           ))}
         </Box>
       </HStack>
+
+      <Box style={{ width: "100%", padding: 20 }}>
+        {activeStep < steps.length ? (
+          steps[activeStep].content
+        ) : (
+          <Box>All Done</Box>
+        )}
+      </Box>
+
       {/* Navigation Buttons */}
       <Flex mt="4" justify="flex-end">
-        {/* <Button onClick={handlePrev} isDisabled={activeStep <= 0} leftIcon={<ChevronLeftIcon />}>
-          Previous
-        </Button> */}
         {activeStep < steps.length ? (
           <Button
             onClick={handleNext}
@@ -171,22 +117,17 @@ const CustomStepperHelper = () => {
             Save & Next
           </Button>
         ) : (
-          <Button
-            onClick={handleNext}
-            rightIcon={<ChevronRightIcon />}
-          >
-            Submit
-          </Button>
+          <Button rightIcon={<ChevronRightIcon />}>Submit</Button>
         )}
       </Flex>
     </>
   );
 };
 
-const CustomStepper = () => {
+const CustomStepper = ({ steps }) => {
   return (
     <ChakraProvider theme={customTheme}>
-      <CustomStepperHelper />
+      <CustomStepperHelper steps={steps} />
     </ChakraProvider>
   );
 };
